e6bddc80324d2123484cc7f1614a437c9d41f1fb
couyang24 <30837746+couyang24@users.noreply.github.com>
2018-05-12 12:12:27 -0400

    code

---
+++ main2.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+library(readtext)
+library(rJava)
+require(qdap)
+library(wordcloud2)
+library(tidyverse)
+
+txt2<-read_tsv("Document1.txt")
+textFreq <- freq_terms(txt2,1000,stopwords = 'en')
+wordcloud2(textFreq, figPath = "ozGWO.png", size = 2.4, color="white", backgroundColor="firebrick")
+
+
