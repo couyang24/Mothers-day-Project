9d18ef17e10cf424af2f92d7520d0fc63dc40c04
Chengran Ouyang <30837746+couyang24@users.noreply.github.com>
2018-05-12 11:16:39 -0400

    init

---
+++ text.R
@@ -0,0 +1,101 @@
+txt <- "Mothers day free sms message
+mothers day text,mothers day free text,mothers day sms,mothers day free sms,mothers day sms collection,free sms mothers day, sms mothers day	
+In this section we present the most largest collection of Happy Mothers Day  SMS ,you can send free SMS Happy Mothers Day . messages to Usa,sms Happy Mothers Day . text message also to Canada,Happy Mothers Day text mothers day sms message to Great Britain,IHappy Mothers Day .India,Happy Mothers Day .Pakistan and worldwide.
+Mommy, I love you For all that you do.Ill kiss you and hug you Cause you love me, too. You feed me and need me To teach you to play,So smile cause I love you.Happy Mothers Day .
+Category  Mothers day text message
+
+What the heart gives away is never gone, but kept in the hearts of others, from dusk to dawn. Love you from the core of my heart. Happy Mothers Day .
+Category  Mothers day text message
+
+M - For the MILLION things she gave me, O - For she's growing OLD, T - For the TEARS she shed to save me, H - For her HEART of purest gold, E - For her EYES, with love-light shining, R - For she is always RIGHT and always be. 
+Category  Mothers day text message
+
+
+
+Golden facts of life...Wen someone luv u...u dont realize it. when u realize it..its too late..U always luv the one who leaves u and leave the one who luvs u.Happy Mothers Day .! 
+Category  Mothers day text message
+
+
+The love she has deep in her heart, Always gives me a good jump start, She is the one whos love is true, Thank u Mom for being u.Happy Mothers Day .
+Category  Mothers day text message
+
+For the best mom who always had a smile for me I know we may be far apart right now So heres a great big hug and kiss Happy Mothers Day .
+Category  Mothers day text message
+
+Happy Mother's Day means more than flowers and gifts It means saying thank you It means I love you You are my mother, my friend Today is your daySo many wonderful moments we have spent together, so many wonderful years in all kinds of weather, thinking of u mother brings memories to mind, wonderful moments i ll treasure, these u gve me sincerely thats why my mom , I love u so dearly. Happy Mothers Day My dear Mom.
+Category  Mothers day text message
+
+who always had a smile for me I know we may be far apart right now So here's a great big hug and kiss Happy Mothers Day .
+Category  Mothers day text message
+
+On this Mothers Day 
+M - For the MILLION things she gave me, 
+O - For she's growing OLD, 
+T - For the TEARS she shed to save me, 
+H - For her HEART of purest gold, 
+E - For her EYES, with love-light shining, 
+R - For she is always RIGHT and always be. 
+Category  Mothers day text message
+
+Thanku mother for all that u do, i know my thanks are far to few, I really do love u i dont say enough, 4 me those words r really tough, i wish i could express all that i feel, bt 4 me u know thats a big deal, Have a g8 day i m thinking of u , i hope my feelings r getting through. Happy Mothers Day .
+Category  Mothers day text message
+
+
+mothers day text,mothers day free text,mothers day sms,mothers day free sms,mothers day sms collection,free sms mothers day, sms mothers day	
+When u feel u r alone in a crowd, when u think no one can understand u, when u think ur love is rejected by others, and when u hate ur life just close ur eyes and think about her who loves u truly, cares for u in ur loneliness, dies when u cry, she is no one else but ur MOM...Happy Mothers Day .
+Category  Mothers day text message
+
+
+
+
+
+
+Give your mother a little something to show that you are thinking about her, and youll be sure to make her smile.
+Category  Mothers day text message
+
+Thanku mother for all that u do, i know my thanks are far to few, I really do love u i dont say enough, 4 me those words r really tough, i wish i could express all that i feel, bt 4 me u know thats a big deal, Have a g8 day i m thinking of u , i hope my feelings r getting through. Happy Mothers Day .
+Category  Mothers day text message
+
+For the best mom who always had a smile for me I know we may be far apart right now So heres a great big hug and kiss Happy Mothers Day .
+Category  Mothers day text message
+
+
+
+Make for me a place within your heart,On which I can depend. For only you Touch the ancient wellsprings of my tears, Home through all the wanderings of my years, Eden that no other can renew, Root I cannot rend through rage or art. 
+Category  Mothers day text message
+
+Youve seen me laugh Youve seen me cry And always you were there with me I may not have always said it But thanks and I love you Happy Mothers Day .
+Category  Mothers day text message
+
+Make for me a place within your heart On which I can depend. For only you Touch the ancient wellsprings of my tears, Home through all the wanderings of my years, Eden that no other can renew, Root I cannot rend through rage or art. 
+Category  Mothers day text message
+
+So many wonderful moments we have spent together, so many wonderful years in all kinds of weather, thinking of u mother brings memories to mind, wonderful moments i ll treasure, these u gve me sincerely thats why my mom , I love u so dearly. Happy Mothers Day 
+Category  Mothers day text message
+
+You've seen me laugh You've seen me cry And always you were there with me I may not have always said it But thanks and I love you Happy Mothers Day .
+Category  Mothers day text message
+
+Being a great mother is a very hard role, but mother u r the star for this one I know, I love you Mom. Happy Mothers Day .
+Category  Mothers day text message
+
+Mommy, I love you For all that you do.I'll kiss you and hug you 'Cause you love me, too.You feed me and need me To teach you to play,So smile 'cause I love you On this Mother's Day. Mommy, I love you.Happy Mothers Day .
+Category  Mothers day text message
+
+A mother serves her sugar with A bit of peppermint To clarify the passages That carry what she meant When she first set to bear a soul Quite separate from her own, Whom she would cherish, yet must teach To live and die alone. 
+Category  Mothers day text message
+
+The full form ofMUMMY
+M-Maa
+U-U Live
+M-Many
+M-More
+Y-Years
+Forward this msg to spread this Dua 4 ur mom 2
+make her Live Long
+May Allah Bless Our Mothers.... 
+Category  Mothers day text message
+
+Only luv never anger Teaching me Helping me Every smile when I was sad Raising me to be strong It spells Mother. Thanks for being u.Happy Mothers Day .
+Category  Mothers day text message
+"
