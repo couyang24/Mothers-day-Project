e2a302a84f0a42ccde3288af439c5f90fe4dcadc
Chengran Ouyang <30837746+couyang24@users.noreply.github.com>
2018-05-12 16:27:53 -0400

    update

--- main2.R
+++ main2.R
@@ -7,6 +7,6 @@ library(tidyverse)
 
 txt2<-read_tsv("Document1.txt")
 textFreq <- freq_terms(txt2,1000,stopwords = 'en')
-wordcloud2(textFreq, figPath = "flower.jpg", size = 2.4, color="white", backgroundColor="firebrick")
+wordcloud2(textFreq, figPath = "m.jpg", size = 2.4, color="white", backgroundColor="firebrick")
 
 
