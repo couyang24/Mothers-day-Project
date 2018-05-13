6e3c6948f405dfea99fe13be4bb0192db23d3225
couyang24 <30837746+couyang24@users.noreply.github.com>
2018-05-13 02:45:53 -0400

    code

--- main2.R
+++ main2.R
@@ -5,8 +5,8 @@ require(qdap)
 library(wordcloud2)
 library(tidyverse)
 
-txt2<-read_tsv("Document1.txt")
-textFreq <- freq_terms(txt2,1000,stopwords = 'en')
-wordcloud2(textFreq, figPath = "m.jpg", size = 2.4, color="white", backgroundColor="firebrick")
-
+txt2<-read_tsv("MothersDayTxt.txt")
+textFreq <- freq_terms(txt2,1000,stopwords = c('en','of','the','for','to','and','text','that'))
+wordcloud2(textFreq, figPath = "rabbit.png", size = 2.9, color="white", backgroundColor="firebrick")
+8
 
